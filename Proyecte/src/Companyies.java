c51f28e48aa4d4d7e5a0d5d2bd49e0207d0c7168
alenf <alenf@Senapo>
2024-11-11 16:39:12 +0100

    Subo el companyies que ruben no sabe programar el pobre

---
+++ Proyecte/src/Companyies.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
+public class Companyies {
+
+    public static void main(String[] args) {
+    	
+        System.out.println(companyia());
+    }
+    
+    // Function that returns a random company name from the file
+    private static String companyia() {
+        Random random = new Random();
+        String[] lineaCompanyia = new String[161];  // Adjust array size based on the file content
+        try (BufferedReader br = new BufferedReader(new FileReader("companiascatalan.txt"))) {
+            String temporal;
+            int cont = 0;
+
+            // Read lines and store in array
+
+            while ((temporal = br.readLine()) != null) {
+                if (cont < lineaCompanyia.length) {
+                    lineaCompanyia[cont] = temporal;
+                    cont++;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Return a random company name
+        if (lineaCompanyia.length > 0) {
+            return lineaCompanyia[random.nextInt(lineaCompanyia.length)];
+        }
+        return null;  // If no companies were loaded
+    }
+}
